1426cb7db4a8e29852132f2ca00c0f559296991e
maddires-del <maddires@mail.gvsu.edu>
2026-05-13 15:09:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if st.button("Analyze"):
 
         genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-2.0-flash")
 
         prompt = f"""
         You are an AI career assistant.
