0e262cfdd83d468dbc4dab7313bee5bf1e6d4281
maddires-del <maddires@mail.gvsu.edu>
2026-05-20 16:33:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import streamlit as st
+import google.generativeai as genai
+
+st.set_page_config(page_title="AI Internship Resume Matcher")
+
+st.title("AI Internship Resume Matcher for Data Science & Generative AI Roles")
+
+st.write("Paste your resume and internship job description below.")
+
+resume = st.text_area("Paste Resume Here", height=250)
+
+job_description = st.text_area("Paste Job Description Here", height=250)
+
 system_prompt = """
 You are an AI career assistant specialized in Data Science, Machine Learning, and Generative AI internships.
 
@@ -32,7 +45,20 @@ Generative AI
 Communication
 Problem Solving
 """
-prompt = f"""
+
+if st.button("Analyze"):
+
+    if not resume or not job_description:
+        st.warning("Please fill both fields.")
+
+    else:
+        genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+
+        model = genai.GenerativeModel("gemini-2.5-flash-lite")
+
+        prompt = f"""
+{system_prompt}
+
 Required Internship Skills:
 {required_skills}
 
@@ -41,6 +67,30 @@ Resume:
 
 Job Description:
 {job_description}
+
+Return output in this format:
+
+1. Match Score: X/100
+
+2. Matching Skills:
+- skill 1
+- skill 2
+
+3. Missing Skills:
+- skill 1
+- skill 2
+
+4. Resume Improvements:
+- suggestion 1
+- suggestion 2
+
+5. Interview Questions:
+- question 1
+- question 2
 """
 
-response = model.generate_content(prompt)
+        response = model.generate_content(prompt)
+
+        st.subheader("Analysis")
+
+        st.write(response.text)
